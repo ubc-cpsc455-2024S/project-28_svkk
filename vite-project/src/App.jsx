91724174a2ba8bd8047f9f47e6ba69cfd558d61b
KurtisHo <Kurtisjeremyho@hotmail.com>
2024-06-06 19:55:36 -0700

    navbar component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import "./components/Navbar.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,7 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
 export default App
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,5 +1,24 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
 export default function Navbar() {
+
     return(
-        <h1>Nav bar</h1>
+        <nav className={"navBar"}>
+            <div className={"navBar-Left"}>
+                <h1> Job Application</h1>
+            </div>
+            <div className={"navBar-Right"}>
+                <div className={"navBar-Buttons"}>
+                    <button className={"navBar-Button"}> Create Cover Letter</button>
+                    <button className={"navBar-Button"}> Sign out</button>
+                </div>
+                <div className={"searchBar-wrapper"}>
+                    <span className="searchIcon">&#128269;</span>
+                    <input type={"text"} className={"searchBar"} placeholder={"Search"}></input>
+                </div>
+            </div>
+        </nav>
     );
 }
