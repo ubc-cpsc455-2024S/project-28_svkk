d53cf86ed96ff1279a417f5e67420e6f588d3221
kevinpoon <kp1998@students.cs.ubc.ca>
2024-06-07 12:24:35 -0700

    Added the CoverLetterPage component and two smaller components (DropdownSelector, WhitePageDisplay).

---
+++ vite-project/src/components/DropdownSelector.jsx
@@ -0,0 +1,35 @@
+export default function DropdownSelector({ allElements, setSelectedElement }) {
+
+    // If the element has a .target property, then it is an event, we need .target.value.
+    // Otherwise, the element itself is in the proper format.
+    function handleClickOption(element) {
+        let selectedValue;
+        if (element.target) {
+            selectedValue = element.target.value;
+        } else {
+            selectedValue = element;
+        }
+        setSelectedElement(selectedValue);
+    }
+
+    // If allElements isn't valid (undefined, null, or empty, display the custom message
+    if (!allElements || allElements.length === 0) {
+        return (
+            <select>
+                <option>Please add a document</option>
+            </select>
+        );
+    } else {
+        // if allElements is valid, then display every element's name in a dropdown menu, and
+        // set up the onClick callback function.
+        return (
+            <select  onChange={handleClickOption}>
+                {allElements.map((element) => (
+                    <option value={element.name} onClick={() => handleClickOption(element.name)}>
+                        {element.name}
+                    </option>
+                ))}
+            </select>
+        );
+    }
+}
---
+++ vite-project/src/components/WhitePageDisplay.jsx
@@ -0,0 +1,37 @@
+export default function WhitePageDisplay({ displayText }) {
+
+    // Checking if the displayText is valid
+    if (displayText !== undefined && displayText !== null && displayText !== "") {
+
+        // Checking if displayText is an object
+        if (displayText.content) {
+            return (
+                <pre>
+                {displayText.content}
+            </pre>
+            );
+        } else {
+            // displayText is a large or small and displayed differently (Can be changed later)
+            if (displayText.length > 200) {
+                return (
+                    <pre>
+                {displayText}
+            </pre>
+                );
+            } else {
+                return (
+                    <div className="group_member_div">
+                        <div className="small-rounded-textbox">
+                            {displayText}
+                        </div>
+                    </div>
+                );
+            }
+        }
+    }
+    return (
+        <div>
+
+        </div>
+    );
+}
