88c274d0e355fa96bd9df52a45380a1af0496f7b
VishalV1807 <vishalvaswani8101@gmail.com>
2024-06-06 21:14:28 -0400

    Made login and signup components

---
+++ vite-project/src/components/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex flex-col border-solid rounded-lg border-neutral-400 border-[1px] p-[5px] pb-[15px] w-[420px] m-[auto] mt-[200px]'>
+      <div className='mt-2'><h2 className='text-3xl'>Application Tailor</h2></div>
+      <div className='flex flex-col h-[200px] justify-center'>
+        <label>
+            Username:
+            <input type="text" name="" id="" placeholder='Username' className='m-4 border-solid rounded-lg border-neutral-500 border-[1px] p-[5px]'/>
+        </label>
+        <label>
+            Password:
+            <input type="text" name="" id="" placeholder='Password' className='m-4 border-solid rounded-lg border-neutral-500 border-[1px] p-[5px]'/>
+        </label>
+      </div>
+      <div>
+        <input className="mb-[10px] w-[100px] h-[35px] bg-custom-blue rounded-md" type="button" value="Log in"/>
+      </div>
+        <p>Already a member? <a className='text-neutral-500 cursor-pointer'>Sign up</a></p>
+    </div>
+  )
+}
+
+export default Login
