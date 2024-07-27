b40040f6298ff661ef11d168e15f7dc198c14b71
VishalV1807 <vishalvaswani8101@gmail.com>
2024-07-27 18:08:06 -0400

    navigate to login page if userEmail is null

--- vite-project/src/components/MainDashboard.jsx
+++ vite-project/src/components/MainDashboard.jsx
@@ -2,13 +2,18 @@ import { useDispatch, useSelector } from "react-redux";
 import JobsContainer from "./JobsContainer";
 import Navbar from "./Navbar";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function MainDashboard() {
 
+    const navigate = useNavigate()
     const userEmail = useSelector(state => state.userEmail.userEmail)
     const dispatch = useDispatch();
 
     useEffect(() => {
+        if (userEmail === "nothing") {
+            navigate("/login")
+        }
         console.log(userEmail)
     }, [])
 
