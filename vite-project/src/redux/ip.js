58bb7d596556a1e4c79072e6a1c578196c01b628
Silvana Huang <silvanahuang23@gmail.com>
2024-08-06 16:47:28 -0700

    updated backend url

--- vite-project/src/redux/ip.js
+++ vite-project/src/redux/ip.js
@@ -2,6 +2,6 @@
 const AWS_IP = "http://44.211.67.73:3000/";
 const LOCAL_IP = "http://localhost:3000/";
 const AWS_ELB = "https://MyAppBackendALB-810126560.us-east-1.elb.amazonaws.com:3000/"
-const RENDER_IP = "https://project-28-svkk-1.onrender.com/";
+const RENDER_IP = "https://project-28-svkk.onrender.com/"
 
 export const USED_IP = RENDER_IP;
