1873270ee3e1a7deb5663f4c57fae416df38ab02
boopchoncc <kevinp@live.ca>
2024-08-06 19:59:30 -0700

    Added AWS_ELB

--- vite-project/src/redux/ip.js
+++ vite-project/src/redux/ip.js
@@ -3,5 +3,6 @@ const AWS_IP = "http://44.211.67.73:3000/";
 const LOCAL_IP = "http://localhost:3000/";
 const AWS_ELB = "https://MyAppBackendALB-810126560.us-east-1.elb.amazonaws.com:3000/"
 const LOCAL_HTTPS_IP = "https://localhost:3000/";
+const AWS_BACKEND = "https://cpsc455-project-backend.com:3000/";
 
-export const USED_IP = AWS_ELB;
+export const USED_IP = AWS_BACKEND;
